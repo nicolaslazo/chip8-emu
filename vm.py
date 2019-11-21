d8e7170d6154b50570f32b047ad5d6e6ed1aedcb
nicolaslazo <nlazo@est.frba.utn.edu.ar>
2019-11-20 22:11:54 -0300

    Implement MemoryBuffer.__setitem__()

--- vm.py
+++ vm.py
@@ -15,6 +15,11 @@ class MemoryBuffer(program):
         binary_data = data_to_binary(program)
         self.memory[512:512+len(binary_data)] = binary_data
 
+    def __setitem__(self, index, data):
+        binary_data = data_to_binary(data)
+        self.memory = self.memory[:index] + binary_data + self.memory[index+len(data)]
+
+
 class Chip8():
     '''Emulated Chip-8 machine.'''
     def __init__(self, program):
