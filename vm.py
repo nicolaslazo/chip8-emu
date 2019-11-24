f0d9641f6637ace7e33b88a86d269536931fcb6e
nicolaslazo <nlazo@est.frba.utn.edu.ar>
2019-11-24 19:20:40 -0300

    Implement up to SUB Vx, Vy

--- vm.py
+++ vm.py
@@ -24,7 +24,7 @@ class MemoryBuffer:
             slice_size = subscript.stop - subscript.start
             data = data[:slice_size]  # Truncates the data to fit in the slice
 
-            self.memory = self.memory[:subscript.start] + data + [subscript.stop:]
+            self.memory = self.memory[:subscript.start] + data + self.memory[subscript.stop:]
         else:
             self.memory = self.memory[:subscript] + data + self.memory[subscript+1:]
 
@@ -64,6 +64,150 @@ class Chip8:
         # Display
         self.screen = [[0] * 64] * 32
 
+        # Opcode categories
+        self.instruction_category = {
+            '0': self.instruction_0,
+            '1': self.instruction_1,
+            '2': self.instruction_2,
+            '3': self.instruction_3,
+            '4': self.instruction_4,
+            '5': self.instruction_5,
+            '6': self.instruction_6,
+            '7': self.instruction_7,
+            '8': self.instruction_8,
+            '9': self.instruction_9,
+            'a': self.instruction_a,
+            'b': self.instruction_b,
+            'c': self.instruction_c,
+            'd': self.instruction_d,
+            'e': self.instruction_e,
+            'f': self.instruction_f
+        }
+
+    # Opcode implementations
+    def execute_instruction(self, instruction):
+        '''
+        Given the instruction xyzw, calls the function instruction_x with yzw as an argument. Serves as a way to call a search tree.
+        '''
+        self.instruction_category[instruction[0]](instruction[1:])
+
+    def instruction_0(self, arg):
+        '''Redirects to 0nnn [SYS addr], 00E0 [CLS] or 00EE [RET].'''
+        if arg == '0e0':
+            self.instruction_00e0() 
+        elif arg == '0ee':
+            self.instruction_00ee()
+        else:
+            self.instruction_0nnn(arg)
+
+    def instruction_00e0(self):
+        '''Instruction 00E0 [CLS].'''
+        self.screen = [[0] * 64] * 32
+
+    def instruction_00ee(self):
+        '''Instruction 00EE [RET].'''
+        return_address = self.pop_from_stack()
+        self.reg_pc = return_address
+
+    def instruction_0nnn(addr):
+        '''Instruction 0nnn [SYS addr].'''
+        self.reg_pc = int(addr)  # This instruction is supposedly ignored by modern interpreters so I might delete it later
+
+    def instruction_1(self, addr):
+        '''Instruction 1nnn [JP addr].'''
+        self.reg_pc = int(addr)
+
+    def instruction_2(self, addr):
+        '''Instruction 2nnn [CALL addr].'''
+        self.push_to_stack(self.reg_pc)
+        self.reg_pc = int(addr)
+
+    def instruction_3(self, arg):
+        '''Instruction 3xkk [SE Vx, byte].'''
+        x = arg[0]; kk = arg[1:]
+        x = int(x); kk = int(kk)
+        if self.reg_v[x] == kk:
+            self.move_to_next_instruction()
+
+    def instruction_4(self, arg):
+        '''Instruction 4xkk [SNE Vx, byte].'''
+        x = arg[0]; kk = arg[1:]
+        x = int(x); kk = int(kk)
+        if self.reg_v[x] != kk:
+            self.move_to_next_instruction()
+
+    def instruction_5(self, arg):
+        '''Instruction 5xy0 [SE Vx, Vy].'''
+        x, y, _ = arg  # Last nibble is not checked to be zero from now, will change if needed
+        x = int(x); y = int(y)
+        if self.reg_v[x] == self.reg_v[y]:
+            self.move_to_next_instruction()
+
+    def instruction_6(self, arg):
+        '''Instruction 6xkk [LD Vx, byte].'''
+        x = arg[0]; kk = arg[1:]
+        x = int(x); kk = int(kk)
+        self.reg_v[x] = kk
+
+    def instruction_7(self, arg):
+        '''Instruction 7xkk [ADD Vx, byte].'''
+        x = arg[0]; kk = arg[1:]
+        x = int(x); kk = int(kk)
+        self.reg_v[x] += kk
+
+    def instruction_8(self, arg):
+        '''Redirects to 8xy[0-7] and 8xyE.'''
+        functions = {
+            '0': self.instruction_8xy0,
+            '1': self.instruction_8xy1,
+            '2': self.instruction_8xy2,
+            '3': self.instruction_8xy3,
+            '4': self.instruction_8xy4,
+            '5': self.instruction_8xy5,
+            '6': self.instruction_8xy6,
+            '7': self.instruction_8xy7,
+            'e': self.instruction_8xyE
+        }
+
+        (x, y, instruction_byte) = arg
+        x = int(x); y = int(y)
+        functions[instruction_byte](x, y)
+
+    def instruction_8xy0(self, x, y):
+        '''Instruction 8xy0 [LD Vx, Vy].'''
+        self.reg_v[x] = self.reg_v[y]
+
+    def instruction_8xy1(self, x, y):
+        '''Instruction 8xy1 [OR Vx, Vy].'''
+        self.reg_v[x] = self.reg_v[x] | self.reg_v[y]
+
+    def instruction_8xy2(self, x, y):
+        '''Instruction 8xy2 [AND Vx, Vy].'''
+        self.reg_v[x] = self.reg_v[x] & self.reg_v[y]
+
+    def instruction_8xy3(self, x, y):
+        '''Instruction 8xy3 [XOR Vx, Vy].'''
+        self.reg_v[x] = self.reg_v[x] ^ self.reg_v[y]
+
+    def instruction_8xy4(self, x, y):
+        '''Instruction 8xy4 [ADD Vx, Vy].'''
+        self.reg_v[x] += self.reg_v[y]
+
+        if self.reg_v[x] > 255:
+            self.reg_v[x] -= 255
+            self.reg_v[15] = 1
+        else:
+            self.reg_v[15] = 0
+
+    def instruction_8xy5(self, x, y):
+        '''Instruction 8xy5 [SUB Vx, Vy].'''
+        self.reg_v[x] -= self.reg_v[y]
+
+        if x > y:
+            self.reg_v[15] = 1 
+        else:
+            self.reg_v[15] = 0
+
     def move_to_next_instruction(self):
         '''Increases the PC register to point to the next instruction.'''
         self.reg_pc += 16  # Instructions are 2 bytes long
@@ -71,7 +215,7 @@ class Chip8:
     def push_to_stack(self, value):
         '''Pushes a value to the stack.'''
         if self.rg_sp == 15:
-            throw Exception('Full stack.')
+            raise Exception('Full stack.')
 
         self.reg_sp += 1
         self.stack[self.reg_sp] = value
@@ -79,14 +223,8 @@ class Chip8:
     def pop_from_stack(self):
         '''Pops a value from the stack.'''
         if self.rg_sp == 0:
-            throw Exception('Empty Stack')
+            raise Exception('Empty Stack')
 
         return_val = self.stack[self.rg_sp]
         self.rg_sp -= 1
         return return_val
-
-    def execute_instruction(self, instruction):
-        '''
-        Given the instruction xyzw, calls the function instruction_x with yzw as an argument. Serves as an instruction search tree
-        '''
-        self.instruction_function[instruction[0]](instruction[1:])
