09974b6fc8c733ce1ac207f2db555a5b5d579be3
nicolaslazo <nlazo@est.frba.utn.edu.ar>
2019-11-26 18:54:42 -0300

    Make instruction_X methods in Chip8 private

--- vm.py
+++ vm.py
@@ -68,133 +68,133 @@ class Chip8:
         self.screen = [[0] * 64] * 32
 
         # Opcode categories
-        self.instruction_category = {
-            '0': self.instruction_0,
-            '1': self.instruction_1,
-            '2': self.instruction_2,
-            '3': self.instruction_3,
-            '4': self.instruction_4,
-            '5': self.instruction_5,
-            '6': self.instruction_6,
-            '7': self.instruction_7,
-            '8': self.instruction_8,
-            '9': self.instruction_9,
-            'a': self.instruction_a,
-            'b': self.instruction_b,
-            'c': self.instruction_c,
-            'd': self.instruction_d,
-            'e': self.instruction_e,
-            'f': self.instruction_f
+        self._instruction_category = {
+            '0': self._instruction_0,
+            '1': self._instruction_1,
+            '2': self._instruction_2,
+            '3': self._instruction_3,
+            '4': self._instruction_4,
+            '5': self._instruction_5,
+            '6': self._instruction_6,
+            '7': self._instruction_7,
+            '8': self._instruction_8,
+            '9': self._instruction_9,
+            'a': self._instruction_a,
+            'b': self._instruction_b,
+            'c': self._instruction_c,
+            'd': self._instruction_d,
+            'e': self._instruction_e,
+            'f': self._instruction_f
         }
 
     # Opcode implementations
     def execute_instruction(self, instruction):
         '''
-        Given the instruction xyzw, calls the function instruction_x with yzw as an argument.
+        Given the instruction xyzw, calls the function _instruction_x with yzw as an argument.
         Serves as a way to call a search tree.
         '''
-        self.instruction_category[instruction[0]](instruction[1:])
+        self._instruction_category[instruction[0]](instruction[1:])
 
-    def instruction_0(self, arg):
+    def _instruction_0(self, arg):
         '''Redirects to 0nnn [SYS addr], 00E0 [CLS] or 00EE [RET].'''
         if arg == '0e0':
-            self.instruction_00e0()
+            self._instruction_00e0()
         elif arg == '0ee':
-            self.instruction_00ee()
+            self._instruction_00ee()
         else:
-            self.instruction_0nnn(arg)
+            self._instruction_0nnn(arg)
 
-    def instruction_00e0(self):
+    def _instruction_00e0(self):
         '''Instruction 00E0 [CLS].'''
         self.screen = [[0] * 64] * 32
 
-    def instruction_00ee(self):
+    def _instruction_00ee(self):
         '''Instruction 00EE [RET].'''
         return_address = self.pop_from_stack()
         self.reg_pc = return_address
 
-    def instruction_0nnn(self, addr):
+    def _instruction_0nnn(self, addr):
         '''Instruction 0nnn [SYS addr].'''
         # This instruction is supposedly ignored by modern interpreters so I might delete it later
         self.reg_pc = int(addr)
 
-    def instruction_1(self, addr):
+    def _instruction_1(self, addr):
         '''Instruction 1nnn [JP addr].'''
         self.reg_pc = int(addr)
 
-    def instruction_2(self, addr):
+    def _instruction_2(self, addr):
         '''Instruction 2nnn [CALL addr].'''
         self.push_to_stack(self.reg_pc)
         self.reg_pc = int(addr)
 
-    def instruction_3(self, arg):
+    def _instruction_3(self, arg):
         '''Instruction 3xkk [SE Vx, byte].'''
         x = arg[0]; kk = arg[1:]
         x = int(x); kk = int(kk)
         if self.reg_v[x] == kk:
             self.move_to_next_instruction()
 
-    def instruction_4(self, arg):
+    def _instruction_4(self, arg):
         '''Instruction 4xkk [SNE Vx, byte].'''
         x = arg[0]; kk = arg[1:]
         x = int(x); kk = int(kk)
         if self.reg_v[x] != kk:
             self.move_to_next_instruction()
 
-    def instruction_5(self, arg):
+    def _instruction_5(self, arg):
         '''Instruction 5xy0 [SE Vx, Vy].'''
         x, y, _ = arg  # Last nibble is not checked to be zero from now, will change if needed
         x = int(x); y = int(y)
         if self.reg_v[x] == self.reg_v[y]:
             self.move_to_next_instruction()
 
-    def instruction_6(self, arg):
+    def _instruction_6(self, arg):
         '''Instruction 6xkk [LD Vx, byte].'''
         x = arg[0]; kk = arg[1:]
         x = int(x); kk = int(kk)
         self.reg_v[x] = kk
 
-    def instruction_7(self, arg):
+    def _instruction_7(self, arg):
         '''Instruction 7xkk [ADD Vx, byte].'''
         x = arg[0]; kk = arg[1:]
         x = int(x); kk = int(kk)
         self.reg_v[x] += kk
 
-    def instruction_8(self, arg):
+    def _instruction_8(self, arg):
         '''Redirects to 8xy[0-7] and 8xyE.'''
         functions = {
-            '0': self.instruction_8xy0,
-            '1': self.instruction_8xy1,
-            '2': self.instruction_8xy2,
-            '3': self.instruction_8xy3,
-            '4': self.instruction_8xy4,
-            '5': self.instruction_8xy5,
-            '6': self.instruction_8xy6,
-            '7': self.instruction_8xy7,
-            'e': self.instruction_8xyE
+            '0': self._instruction_8xy0,
+            '1': self._instruction_8xy1,
+            '2': self._instruction_8xy2,
+            '3': self._instruction_8xy3,
+            '4': self._instruction_8xy4,
+            '5': self._instruction_8xy5,
+            '6': self._instruction_8xy6,
+            '7': self._instruction_8xy7,
+            'e': self._instruction_8xyE
         }
 
         (x, y, instruction_byte) = arg
         x = int(x, 16); y = int(y, 16)
         functions[instruction_byte](x, y)
 
-    def instruction_8xy0(self, x, y):
+    def _instruction_8xy0(self, x, y):
         '''Instruction 8xy0 [LD Vx, Vy].'''
         self.reg_v[x] = self.reg_v[y]
 
-    def instruction_8xy1(self, x, y):
+    def _instruction_8xy1(self, x, y):
         '''Instruction 8xy1 [OR Vx, Vy].'''
         self.reg_v[x] = self.reg_v[x] | self.reg_v[y]
 
-    def instruction_8xy2(self, x, y):
+    def _instruction_8xy2(self, x, y):
         '''Instruction 8xy2 [AND Vx, Vy].'''
         self.reg_v[x] = self.reg_v[x] & self.reg_v[y]
 
-    def instruction_8xy3(self, x, y):
+    def _instruction_8xy3(self, x, y):
         '''Instruction 8xy3 [XOR Vx, Vy].'''
         self.reg_v[x] = self.reg_v[x] ^ self.reg_v[y]
 
-    def instruction_8xy4(self, x, y):
+    def _instruction_8xy4(self, x, y):
         '''Instruction 8xy4 [ADD Vx, Vy].'''
         self.reg_v[x] += self.reg_v[y]
 
@@ -204,7 +204,7 @@ class Chip8:
         else:
             self.reg_v[15] = 0
 
-    def instruction_8xy5(self, x, y):
+    def _instruction_8xy5(self, x, y):
         '''Instruction 8xy5 [SUB Vx, Vy].'''
         # Set VF to NOT borrow
         if self.reg_v[x] > self.reg_v[y]:
@@ -214,7 +214,7 @@ class Chip8:
 
         self.reg_v[x] -= self.reg_v[y]
 
-    def instruction_8xy6(self, x, y):
+    def _instruction_8xy6(self, x, y):
         '''Instruction 8xy6 [SHR Vx {, Vy}].'''
         # Check if the least-significant bit is 1
         if self.reg_v[x] % 2:
@@ -224,7 +224,7 @@ class Chip8:
 
         self.reg_v[x] /= 2
 
-    def instruction_8xy7(self, x, y):
+    def _instruction_8xy7(self, x, y):
         '''Instruction 8xy7 [SUBN Vx, Vy].'''
         # Set VF to NOT borrow
         if self.reg_v[y] > self.reg_v[x]:
@@ -234,7 +234,7 @@ class Chip8:
 
         self.reg_v[x] = self.reg_v[y] - self.reg_v[x]
 
-    def instruction_8xyE(self, x, y):
+    def _instruction_8xyE(self, x, y):
         '''Instruction 8xyE [SHL Vx {, Vy}].'''
         # Check if there's overflow
         if bin(x)[2] == '1':
@@ -244,27 +244,27 @@ class Chip8:
 
         self.reg_v[x] *= 2
 
-    def instruction_9(self, arg):
+    def _instruction_9(self, arg):
         '''Instruction 9xy0 [SNE Vx, Vy].'''
         [x, y, _] = arg
 
         if x != y:
             self.move_to_next_instruction()
 
-    def instruction_a(self, arg):
+    def _instruction_a(self, arg):
         '''Instruction Annn [LD I, addr].'''
         self.reg_i = int(arg, 16)
 
-    def instruction_b(self, arg):
+    def _instruction_b(self, arg):
         '''Instruction Bnnn [JP V0, addr].'''
         self.reg_pc = self.reg_v[0] + int(addr, 16)
 
-    def instruction_c(self, arg):
+    def _instruction_c(self, arg):
         '''Instruction Cxkk [RND Vx, byte].'''
         x = int(arg[0], 16); kk = int(arg[1:], 16)
         self.reg_v[x] = randint(0, 255) & int(kk, 16)
         
-    def instruction_d(self, arg):
+    def _instruction_d(self, arg):
         '''Instruction Dxyn [DRW Vx, Vy, nibble].'''
         [x, y, bytes_to_read] = arg
         x = int(x, 16)
@@ -277,24 +277,24 @@ class Chip8:
 
         self.scr.draw_sprite(self.reg_v[x], self.reg_v[y], sprite)
 
-    def instruction_e(self, arg):
+    def _instruction_e(self, arg):
         '''Redirects to either [SKP Vx] or [SKNP Vx].'''
         x = int(arg[0], 16)
         last_byte = arg[1:]
 
         if last_byte == '9e':
-            self.instruction_Ex9E(self, x)
+            self._instruction_Ex9E(self, x)
         elif last_byte == 'a1':
-            self.instruction_ExA1(self, x)
+            self._instruction_ExA1(self, x)
         else:
             raise Exception('9xXX instruction not recognised.')
 
-    def instruction_Ex9E(self, x):
+    def _instruction_Ex9E(self, x):
         '''Instruction Ex9E [SKP Vx].'''
         if self.scr.key_pressed(self.reg_v[x]):
             self.move_to_next_instruction()
 
-    def instruction_ExA1(self, x):
+    def _instruction_ExA1(self, x):
         '''Instruction ExA1 [SKNP Vx].'''
         if not self.scr.key_pressed(self.reg_v[x]):
             self.move_to_next_instruction()
