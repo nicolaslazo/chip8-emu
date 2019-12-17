4f932a4e16ab82079689af88dec1d62be6c49704
nicolaslazo <nlazo@est.frba.utn.edu.ar>
2019-12-17 10:55:58 -0300

    Add fontset to MemoryBuffer

--- memorybuffer.py
+++ memorybuffer.py
@@ -2,13 +2,31 @@
 '''This module contains the MemoryBuffer class.'''
 
 
+fontset = 'F0909090F0 \
+    2060202070 \
+    F010F080F0 \
+    F010F010F0 \
+    9090F01010 \
+    F080F010F0 \
+    F080F090F0 \
+    F010204040 \
+    F090F090F0 \
+    F090F010F0 \
+    F090F09090 \
+    E090E090E0 \
+    F0808080F0 \
+    E0909090E0 \
+    F080F080F0 \
+    F080F08080'
+
+
 class MemoryBuffer:
     '''Emulated Chip-8 memory.'''
     def __init__(self, program):
         self.memory = ['0'] * 4096
-
         program_size_in_bytes = int(len(program) / 2)
 
+        self[0:80] = fontset
         self[512:512+program_size_in_bytes] = program
 
     def __setitem__(self, subscript, data):
